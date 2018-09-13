aba339ec307c0a116a138df49d9f92978a206e51
mrelli24 <32517753+mrelli24@users.noreply.github.com>
2018-09-13 15:02:40 +0000

    keyrir ekki

---
+++ verk3.py
@@ -0,0 +1,41 @@
+from bottle import *
+
+
+#A liður
+@route("/")
+def index():
+    return """
+    <h2>Verkefni 3</h2>
+    <p> <a href="/a">Liður A</a></p>
+    <p> <a href="/b">Liður B</a></p>
+"""
+
+@route("/a")
+def index():
+    return template("temp-A.tpl")
+
+@route("/sida/<kt>")
+def page(kt):
+    return template("temp-kt.tpl", kt=kt)
+
+#B liður
+
+@route("/Static/<skra>")
+def stati_skra(skra):
+    return static_file(skra, root='./static')
+
+@route("/b")
+def index():
+    return template("index.tpl")
+
+
+
+###############################################################################
+
+@error(404)
+def villa(error):
+    return "<h2 style = color:red>Þessi síða finnst ekki</h2>"
+
+
+
+run(host = 'localhost',port= 5000, reloader=True, debug=True)
---
+++ view/temp-A.tpl
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>KT Flakk</title>
+</head>
+<body>
+	<h2>Verkefni 3 A</h2>
+	<p>Daníel Galvez<a href="/sida/1164642119"></a></p>
+	<p>Hilmir Galvez<a href="/sida/1234567890"></a></p>
+</body>
+</html>
